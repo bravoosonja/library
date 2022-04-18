20649643116709e68fc94d4c8ef3047a76979bf8
bravoosonja <bravoosonja@gmail.com>
2022-04-18 18:06:31 +1000

    complete add book card

--- README.md
+++ README.md
@@ -1,13 +1,20 @@
 # Introduction
-Small library app to keep track of books in the local storage.
+Small library app to keep track of information of your books in the local storage.
 [👉Live Demo]()
 This app features: 
 - add or delete book
 - keeps track of the book title, author, pages, whether or not you have read it yet
+- toggle whether or not you have read the book
+- save, extract, modify book information from the local storage
 # Skills used
 - **JavaScript** objects and object constructors
+- **JavaScript** ```JSON.stringify``` and ```JSON.parse```
 # Lessons learned
 - using modal
+- DOM input checkbox
 # Future plans
+- [ ] refactor code
 - [ ] update Readme
+- [ ] handle keyboard input to close modal
+- [ ] display library status of numbers of books read, not read, and a total number of books
 - [ ] save data on Firebase & enable user authentication
--- script.js
+++ script.js
@@ -5,38 +5,147 @@ const newBtn = document.getElementById("new-book-btn");
 const closeBtn = document.getElementById("close-btn");
 const deleteBtn = document.getElementById("delete-btn");
 const readBtn = document.createElement("button");
+const addBtn = document.getElementById("add-book-btn");
 
 //event listeners
 newBtn.addEventListener("click", openModal);
 closeBtn.addEventListener("click", closeModal);
-deleteBtn.addEventListener("click", deleteBook);
-readBtn.addEventListener("click", updateBook);
+readBtn.addEventListener("click", updateRead);
+addBtn.addEventListener("click", addBookToLibrary);
+window.addEventListener("onload", populateLibrary);
 
 //constructors
-let myLibrary = [
-  {
-    title: "The Hobbit",
-    author: "J.R.R.Tolkin",
-    pages: 295,
-    read: false,
-  },
-  JSON.parse(localStorage.getItem("myLibrary")),
-];
-
 class Book {
-  constructor(title, author, pages, read) {
+  constructor(title, author, pages, isRead) {
     this.title = title;
     this.author = author;
     this.pages = pages;
-    this.read = read;
+    this.isRead = isRead;
   }
 }
 
-//helper function
-function addNewBookToLibrary(newBook) {
-  newBook = new Book(title, author, pages, read);
+let myLibrary = [];
+
+function addBookToLibrary() {
+  const title = document.getElementById("title").value;
+  const author = document.getElementById("author").value;
+  const pages = document.getElementById("pages").value;
+  const isRead = document.getElementById("read").checked;
+  newBook = new Book(title, author, pages, isRead);
   myLibrary.push(newBook);
+  saveBookToLocalStorage();
+  createBookCard(newBook);
+  closeModal();
+}
+
+function populateLibrary() {
+  const booksGrid = document.getElementById("books-grid");
+  booksGrid.querySelectorAll("div").forEach((n) => n.remove());
+
+  for (i = 0; i < myLibrary.length; i++) {
+    createBookCard(myLibrary[i]);
+  }
+}
+
+// const populateLibrary = () => {
+//   const booksGrid = document.getElementById("books-grid");
+//
+//   for (let book of myLibrary.books) {
+//     createBookCard(book);
+//   }
+// };
+
+const createBookCard = (newBook) => {
+  const booksGrid = document.getElementById("books-grid");
+  const bookCard = document.createElement("div");
+  const title = document.createElement("p");
+  const author = document.createElement("p");
+  const pages = document.createElement("p");
+  const btnsGroup = document.createElement("div");
+  const readBtn = document.createElement("button");
+  const deleteBtn = document.createElement("button");
+
+  bookCard.classList.add("book-card");
+  title.classList.add("book-title");
+  btnsGroup.classList.add("btns-container");
+  readBtn.classList.add("normal-btn");
+  deleteBtn.classList.add("normal-btn");
+
+  title.textContent = `${newBook.title}`;
+  author.textContent = `${newBook.author}`;
+  pages.textContent = `${newBook.pages} pages`;
+  deleteBtn.textContent = "Delete";
+
+  if (newBook.isRead === true) {
+    readBtn.textContent = "Read";
+    readBtn.style.backgroundColor = "#c7f9cc";
+    readBtn.style.color = "ffffff";
+  } else if (newBook.isRead === false) {
+    readBtn.textContent = "Not read";
+    readBtn.style.backgroundColor = "#f7b267";
+    readBtn.style.color = "ffffff";
+  }
+
+  deleteBtn.addEventListener("click", () => {
+    myLibrary.splice(myLibrary.indexOf(item), 1);
+    saveBookToLocalStorage();
+    populateLibrary();
+  });
+
+  bookCard.appendChild(title);
+  bookCard.appendChild(author);
+  bookCard.appendChild(pages);
+  btnsGroup.appendChild(readBtn);
+  btnsGroup.appendChild(deleteBtn);
+  booksGrid.appendChild(bookCard);
+  bookCard.appendChild(btnsGroup);
+};
+
+const addBook = () => {
+  // e.preventDefault();
+  const newBook = getBookFromInput();
+  myLibrary.addBook(newBook);
+  saveBookToLocalStorage();
+  updateBooks();
+};
+
+function updateRead() {
+  readBtn.addEventListener("click", () => {
+    if (isRead.value === false) {
+      readBtn.innerHTML = "Not read";
+      readBtn.style.backgroundColor = "#f77f00";
+      readBtn.style.color = "ffffff";
+      localStorage.setItem("myLibrary", JSON.stringify(myLibrary));
+    } else {
+      readBtn.innerHTML = "Read";
+      readBtn.style.backgroundColor = "#0cce6b";
+      readBtn.style.color = "ffffff";
+      localStorage.setItem("myLibrary", JSON.stringify(myLibrary));
+    }
+    saveBookToLocalStorage();
+    populateLibrary();
+  });
+}
+
+// function deleteBook() {
+//   bookGrid.removeChild(bookCard);
+//   myLibrary.splice(bookCard, 1);
+//   localStorage.setItem("myLibrary", JSON.stringify(myLibrary));
+// }
+
+//local storage
+function saveBookToLocalStorage() {
   localStorage.setItem("myLibrary", JSON.stringify(myLibrary));
+}
+
+function getBooksFromLocalStorage() {
+  if (myLibrary === null) {
+    myLibrary = [];
+  } else {
+    const booksFromStorage = JSON.parse(localStorage.getItem("books"));
+    myLibrary = booksFromStorage;
+    populateLibrary();
+  }
 }
 
 // modal
@@ -47,78 +156,4 @@ function openModal() {
 function closeModal() {
   modal.classList.remove("active");
   form.reset();
-}
-
-//main functions
-function addBook(i) {
-  const bookGrid = document.querySelector("book-grid");
-  bookGrid.appendChild(bookCard);
-  const bookCard = document.createElement("div");
-  bookCard.classList.add("book-card");
-  bookCard.setAttribute("data-index", `${i}`);
-
-  const title = document.getElementById("title").value;
-  const titleNode = document.createElement("p");
-  titleNode.classList.add("book-title");
-  titleNode.innerHTML = `${title}`;
-  bookCard.appendChild(titleNode);
-
-  const author = document.getElementById("author").value;
-  const authorNode = document.createElement("p");
-  authorNode.innerHTML = `${author}`;
-  bookCard.appendChild(authorNode);
-
-  const pages = document.getElementById("pages").value;
-  const pagesNode = document.createElement("p");
-  pagesNode.innerHTML = `${pages} pages`;
-  bookCard.appendChild(pagesNode);
-
-  const btnsGroup = document.createElement("div");
-  const isRead = document.getElementById("read").value;
-
-  readBtn.classList.add("normal-btn");
-  btnsGroup.appendChild(readBtn);
-
-  if (isRead.value === false) {
-    readBtn.textContent = "Not read";
-    readBtn.style.backgroundColor = "#f77f00";
-    readBtn.style.color = "ffffff";
-  } else {
-    readBtn.textContent = "Read";
-    readBtn.style.backgroundColor = "#0cce6b";
-    readBtn.style.color = "ffffff";
-  }
-
-  deleteBtn.textContent = "Delete";
-  deleteBtn.classList.add("normal-btn");
-  btnsGroup.appendChild(deleteBtn);
-
-  deleteBtn.addEventListener("click", () => {
-    myLibrary.splice(myLibrary.indexOf(item), 1);
-  });
-
-  addNewBookToLibrary();
-  closeModal();
-}
-
-function updateBook() {
-  readBtn.addEventListener("click", () => {
-    if (isRead.value === false) {
-      readBtn.innerHTML = "Read";
-      readBtn.style.backgroundColor = "#0cce6b";
-      readBtn.style.color = "ffffff";
-      localStorage.setItem("myLibrary", JSON.stringify(myLibrary));
-    } else {
-      readBtn.innerHTML = "Not read";
-      readBtn.style.backgroundColor = "#f77f00";
-      readBtn.style.color = "ffffff";
-      localStorage.setItem("myLibrary", JSON.stringify(myLibrary));
-    }
-  });
-}
-
-function deleteBook() {
-  bookGrid.removeChild(bookCard);
-  myLibrary.splice(bookCard, 1);
-  localStorage.setItem("myLibrary", JSON.stringify(myLibrary));
 }
